4c209352bc9cd6003810fcd06c3b226644b3e204
Matthieu Biville <mattbvl0104@gmail.com>
2024-01-06 10:33:20 +0100

    Osef

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
+    include_once("controller/Controller.php");
 
-include_once("controller/Controller.php");
-
-$controller = new Controller();
-$controller->invoke();
-
+    $controller = new Controller();
+    $controller->invoke();
 ?>
