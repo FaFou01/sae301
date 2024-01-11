4acc7fadce246ac5ff5cec7b16e39bfc49f111a9
Marcus BONED <marcusbonedth@gmail.com>
2024-01-11 18:33:05 +0100

    page mentions légales

---
+++ view/viewML.php
@@ -0,0 +1,21 @@
+<section id="ml">
+    <h1>Mentions légales</h1>
+    <h3>Responsable du site :</h3>
+    <p>[Nom de l'entreprise / Responsable légal] [Adresse de l'entreprise] [Téléphone de l'entreprise] [E-mail de contact]
+        Numéro d'identification : [Numéro d'identification, par exemple : RCS, SIREN, etc.]
+        Directeur de la publication : [Nom du directeur de la publication]</p>
+    <h3>Hébergement du site :</h3>
+    <p>[Nom de l'hébergeur] [Adresse de l'hébergeur] [Téléphone de l'hébergeur]</p>
+    <h3>Propriété intellectuelle :</h3>
+    <p>Ce site web et son contenu sont la propriété de [Nom de l'entreprise] et sont protégés par les lois sur la propriété intellectuelle. Toute reproduction ou utilisation du contenu sans autorisation écrite est interdite.</p>
+    <h3>Protection des données personnelles :</h3>
+    <p>Les données personnelles collectées via ce site sont utilisées uniquement dans le cadre prévu et sont traitées conformément aux dispositions de notre politique de confidentialité.</p>
+    <h3>Liens externes :</h3>
+    <p>Ce site peut contenir des liens vers des sites externes. Nous déclinons toute responsabilité quant au contenu de ces sites externes, ceux-ci relevant de la responsabilité de leurs propriétaires respectifs.</p>
+    <h3>Cookies :</h3>
+    <p>Ce site utilise des cookies pour améliorer l'expérience utilisateur. En naviguant sur ce site, vous consentez à l'utilisation de cookies conformément à notre politique de cookies.</p>
+    <h3>Limitation de responsabilité :</h3>
+    <p>Nous nous efforçons de fournir des informations précises et à jour sur ce site, mais nous ne garantissons pas l'exactitude, l'exhaustivité ou la pertinence des informations fournies. Nous déclinons toute responsabilité quant à l'utilisation faite des informations présentes sur ce site.</p>
+    <h3>Droit applicable :</h3>
+    <p>Ce site est régi par la loi en vigueur [préciser le pays ou la juridiction compétente].</p>
+</section>
