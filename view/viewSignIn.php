6bba73ca6508dde3ebdbc4d31e50a91a56388413
Marcus BONED <marcusbonedth@gmail.com>
2024-01-11 11:07:15 +0100

    SignIn

---
+++ view/viewSignIn.php
@@ -0,0 +1,26 @@
+<section id="inscription">
+    <h2>Inscrivez-vous</h2>
+    <form action="">
+        <div>
+            <label for="">Prénom :</label>
+            <input type="text" name="prenom" id="prenom">
+            <label for="">Nom :</label>
+            <input type="text" name="nom" id="nom">
+        </div>
+        <div>
+            <label for="">Adresse email :</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div>
+            <label for="">Mot de passe :</label>
+            <input type="text" name="mdp" id="mdp">
+        </div>
+        <div>
+            <label for="">Confirmer le mot de passe :</label>
+            <input type="text" name="confirm" id="confirm">
+        </div>
+        <div>
+            <input type="submit">
+        </div>
+    </form>
+</section>
