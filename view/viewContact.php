b693557d7f89773f7edac9d60f7edb8ac16d1a91
Marcus BONED <marcusbonedth@gmail.com>
2024-01-11 18:15:09 +0100

    page contact

---
+++ view/viewContact.php
@@ -0,0 +1,18 @@
+<section id="contact">
+    <div class="divTitre">
+        <h1>Contactez-nous</h1>
+    </div>
+    <div>
+        <h3>Par téléphone :</h3>
+        <p>Service Client : 02 40 09 74 13 <br>
+            Du lundi au vendredi : 9h - 18h / Le Samedi : 9h - 12h30 (hors jours feriés) <br><br>
+
+            Conseils Beauté : 02 40 96 15 43<br>
+            Du lundi au vendredi : 9h - 19h / Le Samedi : 9h - 17h (hors jours feriés)<br>
+        </p>
+    </div>
+    <div>
+        <h3>Email :</h3>
+        <p>nuances.parfum@gmail.com</p>
+    </div>
+</section>
