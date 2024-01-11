c8d9f64467aac4337103f45f88fd3a95d9d9472f
Marcus BONED <marcusbonedth@gmail.com>
2024-01-11 18:25:57 +0100

    page rgpd

---
+++ view/viewRGPD.php
@@ -0,0 +1,29 @@
+<section id="rgpd">
+    <h1>RGPD</h1>
+    <h3>Politique de confidentialité</h3>
+    <p>Dernière mise à jour : 09/01/2024
+        Nous nous engageons à protéger et à respecter votre vie privée. Cette politique de confidentialité explique comment nous collectons, utilisons, stockons et protégeons vos informations personnelles lorsque vous utilisez nos services ou interagissez avec notre site web</p>
+    <h3>Collecte des informations personnelles</h3>
+    <p>Nous collectons diverses informations lorsque vous interagissez avec nos services. Cela peut inclure, sans s'y limiter, les informations que vous fournissez volontairement lorsque vous :
+        Créez un compte utilisateur
+        Utilisez nos services ou achetez nos produits
+        Contactez notre service client
+        Souscrivez à notre newsletter ou à nos communications marketing
+        Les informations collectées peuvent comprendre votre nom, adresse e-mail, adresse postale, numéro de téléphone, informations de paiement et autres données nécessaires à la fourniture de nos services.
+        Utilisation des informations</p>
+    <h3>Nous utilisons vos informations personnelles pour :</h3>
+    <p>Fournir et améliorer nos services
+        Personnaliser votre expérience et vous fournir un contenu pertinent
+        Communiquer avec vous, notamment pour répondre à vos demandes de renseignements, aux commentaires ou aux questions
+        Vous envoyer des mises à jour, des promotions, des offres spéciales ou d'autres informations sur nos produits et services, sauf si vous choisissez de ne pas recevoir de telles communications</p>
+    <h3>Partage des informations</h3>
+    <p>Nous ne vendons, n'échangeons ni ne louons vos informations personnelles à des tiers sans votre consentement, sauf dans les cas prévus par la loi ou lorsque cela est nécessaire pour fournir nos services (par exemple, avec nos prestataires de services tiers).</p>
+    <h3>Protection des informations</h3>
+    <p>Nous mettons en place des mesures de sécurité appropriées pour protéger vos informations personnelles contre tout accès non autorisé, altération, divulgation ou destruction.</p>
+    <h3>Vos droits</h3>
+    <p>Vous avez le droit d'accéder, de mettre à jour, de corriger ou de supprimer vos informations personnelles. Vous pouvez également vous opposer au traitement de vos données personnelles ou demander la limitation de leur utilisation.</p>
+    <h3>Modifications de la politique de confidentialité</h3>
+    <p>Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Toute modification sera publiée sur cette page avec une date de mise à jour.</p>
+    <h3>Contactez-nous</h3>
+    <p>Si vous avez des questions concernant cette politique de confidentialité ou si vous souhaitez exercer vos droits en matière de protection des données, veuillez nous contacter à nuances.parfum@gmail.com.</p>
+</section>
