5a09a8706e33efcc2f08efdf22deb12396c3f9e9
Marcus BONED <marcusbonedth@gmail.com>
2024-01-11 20:20:48 +0100

    début connexion

---
+++ view/viewConnexion.php
@@ -0,0 +1,61 @@
+<section id="connexion">
+    <div class="divAvance">
+        <svg xmlns="http://www.w3.org/2000/svg" width="349" height="60" viewBox="0 0 349 61" fill="none" style="z-index: 100;" >
+            <path d="M1 55V6C1 3.23858 3.23857 1 6 1H304.158C305.071 1 305.967 1.25 306.748 1.72288L345.262 25.0417C348.396 26.9397 348.492 31.4537 345.44 33.4826L306.812 59.1638C305.991 59.7091 305.028 60 304.043 60H6C3.23858 60 1 57.7614 1 55Z" fill="white" stroke="black"/>
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" font-size="28">Panier</text>
+        </svg>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="349" height="60" viewBox="0 0 349 61" fill="none" style="margin-left: -50px; z-index: 90;">
+            <path d="M1 55V6C1 3.23858 3.23857 1 6 1H304.158C305.071 1 305.967 1.25 306.748 1.72288L345.262 25.0417C348.396 26.9397 348.492 31.4537 345.44 33.4826L306.812 59.1638C305.991 59.7091 305.028 60 304.043 60H6C3.23858 60 1 57.7614 1 55Z" fill="#FFA300" stroke="black"/>
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="white" font-size="28">Connexion</text>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="349" height="60" viewBox="0 0 349 61" fill="none" style="margin-left: -50px; z-index: 80;">
+            <path d="M1 55V6C1 3.23858 3.23857 1 6 1H304.158C305.071 1 305.967 1.25 306.748 1.72288L345.262 25.0417C348.396 26.9397 348.492 31.4537 345.44 33.4826L306.812 59.1638C305.991 59.7091 305.028 60 304.043 60H6C3.23858 60 1 57.7614 1 55Z" fill="white" stroke="black"/>
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" font-size="28">Livraison</text>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="349" height="60" viewBox="0 0 349 61" fill="none" style="margin-left: -50px; z-index: 70;">
+            <path d="M1 55V6C1 3.23858 3.23857 1 6 1H304.158C305.071 1 305.967 1.25 306.748 1.72288L345.262 25.0417C348.396 26.9397 348.492 31.4537 345.44 33.4826L306.812 59.1638C305.991 59.7091 305.028 60 304.043 60H6C3.23858 60 1 57.7614 1 55Z" fill="white" stroke="black"/>
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" font-size="28">Paiement</text>
+        </svg>
+    </div>
+    <div class="divbothconn">
+        <div class="connclassique">
+            <h3>Vous avez un compte ?</h3>
+            <h2>Connectez vous</h2>
+            <form action="">
+                <div>
+                    <label for="">Email</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Mot de passe</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <a href="">Vous n’avez pas de compte ? Créez en un !</a>
+                </div>
+                <div>
+                    <input type="submit">
+                </div>
+            </form>
+        </div>
+            <div class="divider"></div>
+        <div class="connautre">
+            <div>
+                <h3>Se connecter avec un compte externe </h3>
+            </div>
+            <div>
+                <img src="../assets/img/google.png" alt="google">
+                <p>Se connecter avec Google</p>
+            </div>
+            <div>
+                <img src="../assets/img/facebook_bleu.png" alt="facebook">
+                <p>Se connecter avec Facebook</p>
+            </div>
+            <div>
+                <img src="../assets/img/X.png" alt="X">
+                <p>Se connecter avec X</p>
+            </div>
+        </div>
+    </div>
+</section>
--- view/viewTemplateTop.php
+++ view/viewTemplateTop.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="assets/css/<?php echo $css?>.css">
     <title>Nuances - Parfumerie en ligne</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php 
