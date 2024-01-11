dc9b2e85c43d8c8bf5ca7b37ba80fc45dff3dd80
Marcus BONED <marcusbonedth@gmail.com>
2024-01-11 12:19:25 +0100

    Navbar responsive

--- view/viewNavbar.php
+++ view/viewNavbar.php
@@ -1,23 +1,36 @@
 <nav>
+    <div class="burger">&#9776;</div>
     <a href="index.php"><img src="assets/img/logo.png" alt="logo du site" id="logoSite"></a>
-    <ul>
-        <li><a href="index.php?page=">Petits prix</a></li>
-        <li><a href="index.php?page=liste_produits_homme">Produits Homme</a></li>
-        <li><a href="index.php?page=liste_produits_femme">Produits Femme</a></li>
-        <li><a href="index.php?page=">Contact</a></li>
-        <li><a href="index.php?page=panier"><img src="assets/img/panier.png" alt="icône compte" class="icone"></a></li>
-        <li>
-            <?php
-                if($isLoggedIn){
-                    echo '<a href="index.php?page="><img src="assets/img/compte.png" alt="icône compte" class="icone">Mon Compte</a>';
-                }
-                else{
-                    echo '<a href="index.php?page="><img src="assets/img/compte.png" alt="icône compte" class="icone"></a>';
-                }
-            ?>
-        </li>
-    </ul>
+    <div class="nav-links">
+        <ul class="nav-none">
+            <li><a href="index.php?page=">Petits prix</a></li>
+            <li><a href="index.php?page=liste_produits_homme">Produits Homme</a></li>
+            <li><a href="index.php?page=liste_produits_femme">Produits Femme</a></li>
+            <li><a href="index.php?page=">Contact</a></li>
+        </ul>
+        <ul class="icones">
+            <li><a href="index.php?page=panier"><img src="assets/img/panier.png" alt="icône compte" class="icone"></a></li>
+            <li>
+                <?php
+                    if($isLoggedIn){
+                        echo '<a href="index.php?page="><img src="assets/img/compte.png" alt="icône compte" class="icone">Mon Compte</a>';
+                    }
+                    else{
+                        echo '<a href="index.php?page="><img src="assets/img/compte.png" alt="icône compte" class="icone"></a>';
+                    }
+                ?>
+            </li>
+        </ul>
+    </div>
 </nav>
 <div id="code_promo">
     <p>- 10% avec le code : <b>PROMO</b></p>
 </div>
+
+<script>
+$(document).ready(function(){
+    $(".burger").click(function(){
+        $(".nav-none").toggleClass("show");
+    });
+});
+</script>
