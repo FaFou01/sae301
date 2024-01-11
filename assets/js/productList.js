d482866008c4a9edd3ab2b172ed72d7ca6a62f51
Matthieu Biville <mattbvl0104@gmail.com>
2024-01-11 16:16:33 +0100

    Ajout tri par type de produit

--- assets/js/productList.js
+++ assets/js/productList.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function(){
     marqueCochee = [];
     choixPrix = document.getElementsByClassName('choixPrix');
     choixType = document.getElementsByClassName('choixType');
+    typeCoche = [];
     choixFormat = document.getElementsByClassName('choixFormat');
 
     produits = document.getElementsByClassName('product');
@@ -31,6 +32,43 @@ window.addEventListener('load', function(){
                     flag = false;
                     for(k=0;k<marqueCochee.length;k++){
                         if(produits[j].classList.contains(marqueCochee[k])){
+                            flag = true;
+                        }
+                    }
+                    if(flag){
+                        produits[j].style.display = "block";
+                    }
+                    else{
+                        produits[j].style.display = "none";
+                    }
+                }
+            }
+        });
+    }
+    for(i=0;i<choixType.length;i++){
+        li = choixType[i];
+        li.addEventListener('click', function(){
+            if(!typeCoche.includes(this.innerHTML.replaceAll(' ', '_'))){
+                this.style.backgroundColor = "#FFA300";
+                this.style.color = "white";
+                typeCoche.push(this.innerHTML.replaceAll(' ', '_'));
+            }
+            else{
+                this.style.backgroundColor = "white";
+                this.style.color = "black";
+                index = typeCoche.indexOf(this.innerHTML);
+                typeCoche.splice(index, 1);
+            }
+            if(typeCoche.length == 0){
+                for(j=0;j<produits.length;j++){
+                    produits[j].style.display = "block";
+                }
+            }
+            else{
+                for(j=0;j<produits.length;j++){
+                    flag = false;
+                    for(k=0;k<typeCoche.length;k++){
+                        if(produits[j].classList.contains(typeCoche[k])){
                             flag = true;
                         }
                     }
