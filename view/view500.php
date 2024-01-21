31f4cbe4e28fbb1ca56de0a3be66c7527a915e2b
Matthieu Biville <98494589+FaFou01@users.noreply.github.com>
2024-01-21 18:39:33 +0100

    Create view500.php

---
+++ view/view500.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/general.css">
+    <link rel="stylesheet" href="assets/css/404.css">
+    <title>Nuances 404</title>
+</head>
+<body>
+    <div id="header"></div>
+    <main>
+        <div id="title">
+            <img src="assets/img/favicon.png" alt="">
+            <h1>ERREUR 500</h1>
+            <img src="assets/img/favicon.png" alt="">
+        </div>
+        <p>Nous sommes navrés, mais un problème semble avoir eu lieu !</p>
+        <p>Nous vous conseillons de réessayer plus tard.</p>
+    </main>
+    <div id="footer"></div>
+</body>
+</html>
